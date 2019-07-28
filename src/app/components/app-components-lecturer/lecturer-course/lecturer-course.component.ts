ae61e979b5a65f43e32bfba60ffc8f7ab374801a
Aaron Ting <aaronting1998@gmail.com>
2019-07-28 21:50:23 +1000

    parsing class info

--- src/app/components/app-components-lecturer/lecturer-course/lecturer-course.component.ts
+++ src/app/components/app-components-lecturer/lecturer-course/lecturer-course.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ClassUtilService } from 'src/app/services/class-util.service';
+import { ClassModel } from 'src/app/models/classModel';
 
 @Component({
   selector: 'app-lecturer-course',
@@ -10,13 +12,21 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class LecturerCourseComponent implements OnInit {
 
   @Input() course: string;
+  classes: ClassModel[] = [];
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private cuSrv: ClassUtilService) {
     if (this.router.getCurrentNavigation()) {
       this.course = this.router.getCurrentNavigation().extras.state.course;
+      this.cuSrv.searchClasses(this.course);
     } else {
-      this.course = "No Course Selected";
+      this.router.navigate(['/crelture/home'])
     }
+
+    this.cuSrv.searchClasses(this.course).subscribe(
+      response => {
+        this.classes = response;
+      }
+    )
   }
 
   ngOnInit() {
--- src/app/services/class-util.service.ts
+++ src/app/services/class-util.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
+import { ClassModel } from '../models/classModel';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,9 @@ import { BehaviorSubject } from 'rxjs';
 export class ClassUtilService {
 
   searchState: BehaviorSubject<string> = new BehaviorSubject<string>("initial");
+  getSearchState() {
+    return this.searchState;
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -31,38 +35,65 @@ export class ClassUtilService {
   searchCourses(faculty: string, term: string) {
     this.searchState.next("searching")
     var coursesParsed = new BehaviorSubject<string[]>([]);
-    try {
-      this.http.get('https://cors-anywhere.herokuapp.com/http://classutil.unsw.edu.au/' +
-      faculty.toUpperCase() + '_' + term + '.html', { responseType: 'text' }).subscribe(
-        response => {
-          var responseArray = response.match(/<a name=\"[A-Z]{4}.*?<\/td><\/tr>/gs);
-          var courses = []
-          for (let course of responseArray) {
-            course = course.replace(/</g, '');
-            course = course.replace(/>/g, '');
-            course = course.replace(/"/g, '');
-            course = course.replace(/a name=/g, '');
-            course = course.replace(/\/ab.*center/gs, ' ');
-            course = course.replace(/\/td\/tr/g, '');
-            course = course.replace(/amp;/g, '')
-            courses.push(course);
-          }
-          coursesParsed.next(courses);
-          console.log(courses);
-          if (courses.length > 0) {
-            this.searchState.next("found");
-          }
+    this.http.get('https://cors-anywhere.herokuapp.com/http://classutil.unsw.edu.au/' +
+    faculty.toUpperCase() + '_' + term + '.html', { responseType: 'text' }).subscribe(
+      response => {
+        console.log(response);
+        var responseArray = response.match(/<a name=\"[A-Z]{4}.*?<\/td><\/tr>/gs);
+        var courses = []
+        for (let course of responseArray) {
+          course = course.replace(/</g, '');
+          course = course.replace(/>/g, '');
+          course = course.replace(/"/g, '');
+          course = course.replace(/a name=/g, '');
+          course = course.replace(/\/ab.*center/gs, ' ');
+          course = course.replace(/\/td\/tr/g, '');
+          course = course.replace(/amp;/g, '')
+          courses.push(course);
         }
-      )
-    } catch {
-      coursesParsed.next(['error']);
-      return 
-    }
+        coursesParsed.next(courses);
+        console.log(courses);
+        if (courses.length > 0) {
+          this.searchState.next("found");
+        } else {
+          this.searchState.next("notfound");
+        }
+      }, error => {
+        this.searchState.next("error");
+      }
+    );
     return coursesParsed;
   }
 
-  getSearchState() {
-    return this.searchState;
+  searchClasses(course: string) {
+    var classesParsed = new BehaviorSubject<ClassModel[]>([]);
+    var faculty = course.substring(0, 4);
+    var term = course.substring(8, 10);
+    this.http.get('https://cors-anywhere.herokuapp.com/http://classutil.unsw.edu.au/' +
+    faculty.toUpperCase() + '_' + term + '.html', { responseType: 'text' }).subscribe(
+      response => {
+        var regex = new RegExp(course + "\"><\/a.+?<tr><td colspan=2 class=\"cucourse\" align=right valign=center>", "gs");
+        var responseArray = response.match(regex);
+        var classes = responseArray[0].match(/<td.*?>[A-Z]{3}<\/td>.+?<\/td> <\/tr>/gs);
+        var classModels = [];
+        for (let _class of classes) {
+          if (!_class.includes(">CRS<") && !_class.includes(">LEC<")) {
+            var classModel: ClassModel = {
+              type: _class.match(/>[A-Z]{3}</)[0].substring(1,4),
+              session: _class.match(/>[A-Z]\d\d[A-Z]</)[0].substring(1,5),
+              id: _class.match(/> \d{4}</)[0].substring(2,6),
+              status: _class.match(/>[A-Z][a-z]{3,}?</)[0].substring(1).replace("<",""),
+              enrolments: _class.match(/[0-9]+?\/.+?</)[0].replace("<",""),
+              details: _class.match(/<td>(?:(?!<td>).)*?<\/td> <\/tr>$/s)[0].substring(4).replace("<\/td> <\/tr>", "").replace(/<.+?>/gs, "")
+            }
+            classModels.push(classModel);
+          }
+        }
+        classesParsed.next(classModels);
+        console.log(classModels);
+      }
+    );
+    return classesParsed;
   }
 
 }
